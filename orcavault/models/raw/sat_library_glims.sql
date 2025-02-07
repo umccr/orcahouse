c35c99c7ec2d4fe2986bd56523c1fc1b2b8e6563
Victor San Kho Lin <victor@sankholin.com>
2025-02-07 21:40:30 +1100

    Implemented OrcaVault Library satellite model - glims
    * Satellite record source is PSA spreadsheet_google_lims. Hence, shorthand `_glims` suffix.
    * Similarly to #32
      Except, it is retained history as the original Google LIMS interpretation about the Library entity description.
    * Slightly tailored incremental loading strategy needed to use; by ranking with timestamp, illumina_id, workflow
      column combo order to reduce duplicate for the initial data load_datetime.

---
+++ orcavault/models/raw/sat_library_glims.sql
@@ -0,0 +1,117 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='append',
+        on_schema_change='fail'
+    )
+}}
+
+with source as (
+
+    select
+        record_source,
+        library_id,
+        cast("timestamp" as date) as "timestamp",
+        illumina_id,
+        workflow,
+        phenotype,
+        type,
+        assay,
+        quality,
+        source
+    from
+        {{ ref('spreadsheet_google_lims') }}
+
+),
+
+cleaned as (
+
+    select
+        record_source,
+        trim(regexp_replace(library_id, E'[\\n\\r]+', '', 'g')) as library_id,
+        "timestamp",
+        row_number() over (partition by library_id, "timestamp" order by "timestamp" desc, illumina_id desc, workflow desc) as rank,
+        trim(regexp_replace(workflow, E'[\\n\\r]+', '', 'g')) as workflow,
+        trim(regexp_replace(phenotype, E'[\\n\\r]+', '', 'g')) as phenotype,
+        trim(regexp_replace(type, E'[\\n\\r]+', '', 'g')) as type,
+        trim(regexp_replace(assay, E'[\\n\\r]+', '', 'g')) as assay,
+        trim(regexp_replace(quality, E'[\\n\\r]+', '', 'g')) as quality,
+        trim(regexp_replace(source, E'[\\n\\r]+', '', 'g')) as source
+    from
+        source
+    where
+        library_id is not null or library_id <> ''
+
+),
+
+differentiated as (
+
+    select
+        *
+    from
+        cleaned
+    where
+        rank = 1
+    {% if is_incremental() %}
+        and cast("timestamp" as timestamptz) + time '11:00' > ( select coalesce(max(load_datetime), '1900-01-01') as ldts from {{ this }} )
+    {% endif %}
+
+),
+
+encoded as (
+
+    select
+        record_source,
+        encode(sha256(cast(library_id as bytea)), 'hex') as library_hk,
+        encode(sha256(concat("timestamp", workflow, phenotype, type, assay, quality, source)::bytea), 'hex') as hash_diff,
+        "timestamp",
+        workflow,
+        phenotype,
+        type,
+        assay,
+        quality,
+        source
+    from
+        differentiated
+
+),
+
+transformed as (
+
+    select
+        library_hk,
+        cast("timestamp" as timestamptz) + time '11:00' as load_datetime,
+        record_source,
+        hash_diff,
+        "timestamp",
+        workflow,
+        phenotype,
+        type,
+        assay,
+        quality,
+        source
+    from
+        encoded
+
+),
+
+final as (
+
+    select
+        cast(library_hk as char(64)) as library_hk,
+        cast(load_datetime as timestamptz) as load_datetime,
+        cast(record_source as varchar(255)) as record_source,
+        cast(hash_diff as char(64)) as hash_diff,
+        cast("timestamp" as date) as "timestamp",
+        cast(workflow as varchar(255)) as workflow,
+        cast(phenotype as varchar(255)) as phenotype,
+        cast(type as varchar(255)) as type,
+        cast(assay as varchar(255)) as assay,
+        cast(quality as varchar(255)) as quality,
+        cast(source as varchar(255)) as source
+    from
+        transformed
+
+)
+
+select * from final
