35d3686aa2a53134b870f813240c41434ffebb7a
Victor San Kho Lin <victor@sankholin.com>
2025-02-27 20:09:46 +1100

    Improved raw vault s3object hub model - last_seen_date
    * Added `last_seen_date` column to s3object hub model
    * Used `event_time` from FileManager s3object table because
      when an object get deleted, `last_modified_date` column
      reset to null.

--- orcavault/models/raw/hub_s3object.sql
+++ orcavault/models/raw/hub_s3object.sql
@@ -11,14 +11,14 @@ with source as (
     select
         bucket,
         "key",
-        last_modified_date
+        cast(last_modified_date as timestamptz) as last_seen_date
     from
         {{ source('ods', 'data_portal_s3object') }}
     union
     select
         bucket,
         "key",
-        last_modified_date
+        cast(event_time as timestamptz) as last_seen_date
     from
         {{ source('ods', 'file_manager_s3_object') }}
 
@@ -29,8 +29,8 @@ cleaned as (
     select
         bucket,
         "key",
-        last_modified_date,
-        row_number() over (partition by bucket, "key" order by last_modified_date desc) as rank
+        last_seen_date,
+        row_number() over (partition by bucket, "key" order by last_seen_date desc) as rank
     from
         source
 
@@ -45,7 +45,7 @@ differentiated as (
     where
         rank = 1
     {% if is_incremental() %}
-        and cast(last_modified_date as timestamptz) > ( select coalesce(max(load_datetime), '1900-01-01') as ldts from {{ this }} )
+        and cast(last_seen_date as timestamptz) > ( select coalesce(max(load_datetime), '1900-01-01') as ldts from {{ this }} )
     {% endif %}
 
 ),
@@ -56,6 +56,7 @@ transformed as (
         encode(sha256(concat(bucket, "key")::bytea), 'hex') as s3object_hk,
         bucket,
         "key",
+        last_seen_date,
         cast('{{ run_started_at }}' as timestamptz) as load_datetime,
         (select 's3') as record_source
     from
@@ -69,6 +70,7 @@ final as (
         cast(s3object_hk as char(64)) as s3object_hk,
         cast(bucket as varchar(255)) as bucket,
         cast("key" as text) as "key",
+        cast(last_seen_date as timestamptz) as last_seen_date,
         cast(load_datetime as timestamptz) as load_datetime,
         cast(record_source as varchar(255)) as record_source
     from
