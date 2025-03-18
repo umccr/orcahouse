b67019c94751eda8940749f3ca18dd92f291887d
Victor San Kho Lin <victor@sankholin.com>
2025-03-18 11:04:17 +1100

    Refactored DCL layer hub_contact to hub_owner concept
    * Renamed hub_contact to hub_owner concept which suit better for modelling
      business context and warehouse consolidation purpose.
      e.g. link_project_ownership between hub_project to hub_owner
    * It follows business user language "ProjectOwner" from upstream.

---
+++ orcavault/models/dcl/hub_owner.sql
@@ -0,0 +1,67 @@
+{{
+    config(
+        indexes=[
+            {'columns': ['owner_id'], 'type': 'btree'},
+        ],
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='owner_id',
+        on_schema_change='fail'
+    )
+}}
+
+with source as (
+
+    select project_owner as owner_id from {{ source('ods', 'data_portal_labmetadata') }}
+    union
+    select project_owner as owner_id from {{ source('ods', 'data_portal_limsrow') }}
+    union
+    select contact_id as owner_id from {{ source('ods', 'metadata_manager_contact') }}
+    union
+    select project_owner as owner_id from {{ ref('spreadsheet_library_tracking_metadata') }}
+    union
+    select project_owner as owner_id from {{ ref('spreadsheet_google_lims') }}
+
+),
+
+cleaned as (
+
+    select * from source where owner_id is not null and owner_id <> ''
+
+),
+
+differentiated as (
+
+    select owner_id from cleaned
+    {% if is_incremental() %}
+    except
+    select owner_id from {{ this }}
+    {% endif %}
+
+),
+
+transformed as (
+
+    select
+        encode(sha256(cast(owner_id as bytea)), 'hex') as owner_hk,
+        owner_id,
+        cast('{{ run_started_at }}' as timestamptz) as load_datetime,
+        (select 'lab') as record_source
+    from
+        differentiated
+
+),
+
+final as (
+
+    select
+        cast(owner_hk as char(64)) as owner_hk,
+        cast(owner_id as varchar(255)) as owner_id,
+        cast(load_datetime as timestamptz) as load_datetime,
+        cast(record_source as varchar(255)) as record_source
+    from
+        transformed
+
+)
+
+select * from final
