878813e284cee03f2851fec52cc1b1e93d82500c
Victor San Kho Lin <victor@sankholin.com>
2025-04-01 15:20:31 +1100

    Implemented FASTQ History data mart view
    * Listing of Centre genomic sequencing unaligned read level FASTQ S3 historical locations.
      Filter by library_id in WHERE clause.

--- orcavault/macros/grant_select.sql
+++ orcavault/macros/grant_select.sql
@@ -2,6 +2,13 @@
 {% set sql %}
     GRANT USAGE ON SCHEMA mart TO {{ role }};
     GRANT SELECT ON ALL tables IN SCHEMA mart TO {{ role }};
+
+    {# FIXME to be finalised - experimental alias views on public schema for AppSync GraphQL introspection purpose #}
+    CREATE VIEW public.lims as SELECT * FROM mart.lims;
+    CREATE VIEW public.fastq as SELECT * FROM mart.fastq;
+    CREATE VIEW public.fastq_history as SELECT * FROM mart.fastq_history;
+    GRANT USAGE ON SCHEMA public TO {{ role }};
+    GRANT SELECT ON ALL tables IN SCHEMA public TO {{ role }};
 {% endset %}
 
 {% do run_query(sql) %}
