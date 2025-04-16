692c6d590f0a7aef2116233fe8055dc41e4141c6
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-04-16 14:58:24 +1000

    Fix pre-commit errors

--- infra/aurora-su/orcavault_psa.tf
+++ infra/aurora-su/orcavault_psa.tf
@@ -5,5 +5,5 @@ module "psa_rw" {
   db_name = "orcavault"
   stack_name = "OrcaVaultRDSSecretRotationPSA"
   db_user_ssm_parameter = "/orcahouse/orcavault/psa_username"
-  secret_name = "orcahouse/orcavault/psa_rw"
+  secret_name = "orcahouse/orcavault/psa_rw"  # pragma: allowlist secret
 }
