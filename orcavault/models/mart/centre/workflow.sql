253c0c0f9e8f368654fbc6760086529892d28480
Victor San Kho Lin <victor@sankholin.com>
2025-04-29 17:09:33 +1000

    Improved data mart Workflow table
    * Enhanced and simplified data mart Workflow model implementation
      by using business vault satellite from #101
    * Added workflow start and end time dimension
    * Added precomputed workflow duration in seconds
    * Added workflow comment column for any operational note or override
    
    Co-authored-by: @reisingerf

--- orcavault/models/mart/centre/workflow.sql
+++ orcavault/models/mart/centre/workflow.sql
@@ -7,6 +7,10 @@
             {'columns': ['workflow_name'], 'type': 'btree'},
             {'columns': ['workflow_version'], 'type': 'btree'},
             {'columns': ['workflow_status'], 'type': 'btree'},
+            {'columns': ['workflow_start'], 'type': 'btree'},
+            {'columns': ['workflow_end'], 'type': 'btree'},
+            {'columns': ['workflow_start', 'workflow_end'], 'type': 'btree'},
+            {'columns': ['workflow_comment'], 'type': 'btree'},
         ]
     )
 }}
@@ -22,63 +26,27 @@ with linked as (
 
 ),
 
-ranked as (
-
-    select
-        workflow_run_hk,
-        workflow_name,
-        workflow_version,
-        state_timestamp,
-        state_status,
-        row_number() over (partition by workflow_run_hk order by state_timestamp desc) as rank
-    from {{ ref('sat_workflow_run_detail') }}
-
-),
-
-detailed as (
-
-    select * from ranked where rank = 1
-
-),
-
 merged as (
 
-    {# Potentially move this logic into DCL business vault. See https://github.com/umccr/orcahouse/pull/90 #}
-
     select
-        hub.portal_run_id as portal_run_id,
-        case
-            when sat.workflow_name is null then
-                ptl.type_name
-            else
-                sat.workflow_name
-            end as workflow_name,
-        case
-            when sat.state_status is null then
-                ptl.end_status
-            else
-                sat.state_status
-            end as workflow_status,
-        case
-            when sat.workflow_version is null then
-                ptl.version
-            else
-                sat.workflow_version
-            end as workflow_version
-    from {{ ref('hub_workflow_run') }} hub
-        full join detailed sat on sat.workflow_run_hk = hub.workflow_run_hk
-        full join {{ ref('sat_workflow_run_portal') }} ptl on ptl.workflow_run_hk = hub.workflow_run_hk
+        *
+    from dcl.hub_workflow_run hub
+        join dcl.sat_workflow_run sat on sat.workflow_run_hk = hub.workflow_run_hk
 
 ),
 
 transformed as (
 
     select
+        linked.library_id as library_id,
         merged.portal_run_id as portal_run_id,
         merged.workflow_name as workflow_name,
         merged.workflow_version as workflow_version,
-        merged.workflow_status as workflow_status,
-        linked.library_id as library_id
+        upper(merged.workflow_run_status) as workflow_status,
+        merged.workflow_run_start as workflow_start,
+        merged.workflow_run_end as workflow_end,
+        extract(epoch from age(merged.workflow_run_end, merged.workflow_run_start)) as workflow_duration,
+        merged.workflow_run_comment as workflow_comment
     from
         merged
             join linked on linked.portal_run_id = merged.portal_run_id
@@ -92,7 +60,11 @@ final as (
         cast(library_id as varchar(255)) as library_id,
         cast(workflow_name as varchar(255)) as workflow_name,
         cast(workflow_version as varchar(255)) as workflow_version,
-        cast(workflow_status as varchar(255)) as workflow_status
+        cast(workflow_status as varchar(255)) as workflow_status,
+        cast(workflow_start as timestamptz) as workflow_start,
+        cast(workflow_end as timestamptz) as workflow_end,
+        cast(workflow_duration as numeric) as workflow_duration,
+        cast(workflow_comment as text) as workflow_comment
     from
         transformed
     order by portal_run_id desc nulls last, library_id desc
