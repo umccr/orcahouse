8e585536542ed26e795e5e34395ece2bbecad395
Victor San Kho Lin <victor@sankholin.com>
2025-05-17 12:42:29 +1000

    Improved DCL satellite WorkflowRunComment
    * The upstream WorkflowRunComment may add multiple comments within
      a day. Added the sub-sequencing column to track this change history
      based on the hash_diff changes to the satellite model composite PK.

--- orcavault/models/dcl/sat_workflow_run_comment.sql
+++ orcavault/models/dcl/sat_workflow_run_comment.sql
@@ -54,6 +54,7 @@ final as (
 
     select
         cast(workflow_run_hk as char(64)) as workflow_run_hk,
+        cast(hash_diff as char(64)) as workflow_run_sq,
         cast(load_datetime as timestamptz) as load_datetime,
         cast(record_source as varchar(255)) as record_source,
         cast(hash_diff as char(64)) as hash_diff,
