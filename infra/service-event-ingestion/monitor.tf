0d1fd1399cf04690c416f11cd3fbc9db9a640e65
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-09-30 09:29:39 +1000

    Add DLQ CloudWatch alarms

--- infra/service-event-ingestion/monitor.tf
+++ infra/service-event-ingestion/monitor.tf
@@ -43,3 +43,40 @@ module "lambda_error_alarms" {
 
   alarm_actions = [local.error_alarm_target]
 }
+
+module "lambda_dlq_alarms" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarms-by-multiple-dimensions"
+  version = "~> 3.0"
+
+  alarm_name          = "service_ingest_dlq_"
+  alarm_description   = "DLQ messages too high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 1
+  period              = 60
+  unit                = "Count"
+
+  namespace   = "AWS/SQS"
+  metric_name = "NumberOfMessagesReceived"
+  statistic   = "Maximum"
+
+  dimensions = {
+    "wfm_sqs" = {
+      QueueName = "${local.wfm.function_name}_dlq"
+    },
+    "fqr_sqs" = {
+      QueueName = "${local.fqr.function_name}_dlq"
+    },
+    "srm_sc_sqs" = {
+      QueueName = "${local.srm.sc_function_name}_dlq"
+    },
+    "srm_llc_sqs" = {
+      QueueName = "${local.srm.llc_function_name}_dlq"
+    },
+    "mm_lib_sqs" = {
+      QueueName = "${local.mm.lib_function_name}_dlq"
+    }
+  }
+
+  alarm_actions = [local.error_alarm_target]
+}
