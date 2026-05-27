d041cef051011e41139c106dcd3e08ed035c3a0e
Victor San Kho Lin <victor@sankholin.com>
2026-05-28 09:01:38 +1000

    Implemented OncoVault Warehouse bucket
    * This stack provisions the S3 bucket for OncoVault iceberg tables purpose.

---
+++ infra/s3/oncovault/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.15.0"
+
+  backend "s3" {
+    bucket       = "terraform-states-ccfgcm"
+    key          = "115253169271/orcahouse/s3/oncovault/terraform.tfstate"
+    region       = "ap-southeast-2"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+
+  default_tags {
+    tags = {
+      "umccr-org:Product" = "OrcaHouse"
+      "umccr-org:Creator" = "Terraform"
+      "umccr-org:Service" = "OrcaHouse"
+      "umccr-org:Source"  = "https://github.com/umccr/orcahouse"
+    }
+  }
+}
+
+# ---
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
